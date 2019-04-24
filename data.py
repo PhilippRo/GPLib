Fix sign error that causes ugly plots

data.py
@@ -47,7 +47,7 @@ class LinRegResult:
 
 
         res_plot.plot(self.xs, np.zeros(len(self.xs)))
-        res_plot.errorbar(self.xs, self.ys - self.m * self.xs + self.c, ecolor="r",
+        res_plot.errorbar(self.xs, self.ys - self.m * self.xs - self.c, ecolor="r",
             yerr = self.y_err_stat + self.m * self.x_err_stat,
             capsize=3, elinewidth=1, label = r"Residualgraph $f(x) - y$",
                 fmt='', linewidth=0)