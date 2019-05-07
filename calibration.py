9e1bc3f5b0351270528a154ce2f67253883d42df
Jonas Colve <jonas.colve@outlook.de>
2019-05-07 20:45:00 +0200

    Removed correlation between parameters in calibration via substraction of mean values

--- calibration.py
+++ calibration.py
@@ -2,23 +2,28 @@ import numpy as np
 import sympy
 from data import Data
 from data import Expression
+from praktikum import analyse
 
 class Calibration:
     def __init__( self, x_m, x_r ):
         if isinstance( x_m, Expression ) :
-            self.mes = x_m
+            self.mes_mittel = analyse.mittelwert_stdabw( x_m.consume().data )[0]
+            self.mes = x_m - self.mes_mittel
         else :
-            self.mes = Expression( x_m )
+            self.mes_mittel = analyse.mittelwert_stdabw( x_m.data )[0]
+            self.mes = Expression( x_m ) - self.mes_mittel
         if isinstance( x_r, Expression ) :
-            self.real = x_r
+            self.real_mittel = analyse.mittelwert_stdabw( x_r.consume().data )[0]
+            self.real = x_r - self.real_mittel
         else :
-            self.real = Expression( x_r )
+            self.real_mittel = analyse.mittelwert_stdabw( x_r.data )[0]
+            self.real = Expression( x_r ) - self.real_mittel
         self.result = self.mes < self.real
 
     def calibrate( self, x, x_name ) :
         m = Data('m_intern', self.result.m, uncert_sys=self.result.m_err_stat)
         c = Data('c_intern', self.result.c, uncert_sys=self.result.c_err_stat)
-        res = ( m*x + c ).consume(x_name)
+        res = ( m*(x-self.mes_mittel) + c + self.real_mittel ).consume(x_name)
         if isinstance( res.data, np.ndarray ) :
             res.uncert_sys = np.zeros(len(res.data))
         else :
@@ -31,7 +36,12 @@ class Calibration:
         return self.result.slope( symbol )
 
     def axis_intercept(self, symbol) :
-        return self.result.axis_intercept( symbol )
+        c = self.result.axis_intercept( 'c_intern' )
+        m = self.result.axis_intercept( 'm_intern' )
+        ret = ( c - m * self.mes_mittel + self.real_mittel).consume( symbol )
+        ret.blacklist.remove( c )
+        ret.blacklist.remove( m )
+        return ret
 
     def save_to_file(self, filen, xname, yname, font_size, legend_font_size = None,
             tex_file = None, title = None):
