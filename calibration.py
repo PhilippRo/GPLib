f286224aabe4deb0fb619c330aa372c1b3bb0391
Jonas Colve <jonas.colve@outlook.de>
2019-05-09 17:57:31 +0200

    Fixed error propagation in Calibration

--- calibration.py
+++ calibration.py
@@ -5,7 +5,7 @@ from data import Expression
 from praktikum import analyse
 
 class Calibration:
-    def __init__( self, x_m, x_r ):
+    def __init__( self, x_m, x_r, m_symbol, c_symbol ):
         if isinstance( x_m, Expression ) :
             self.mes_mittel = analyse.mittelwert_stdabw( x_m.consume().data )[0]
             self.mes = x_m - self.mes_mittel
@@ -19,25 +19,22 @@ class Calibration:
             self.real_mittel = analyse.mittelwert_stdabw( x_r.data )[0]
             self.real = Expression( x_r ) - self.real_mittel
         self.result = self.mes < self.real
+        self.m = result.slope( c_symbol )
+        self.c = ExpressionSub( self.real_mittel, self.m * self.mes_mittel )
 
     def calibrate( self, x, x_name ) :
-        m = Data('m_intern', self.result.m, uncert_sys=self.result.m_err_stat)
-        c = Data('c_intern', self.result.c, uncert_sys=self.result.c_err_stat)
+        m = Data('m_intern', self.result.m, uncert_sys=np.sqrt( self.result.m_err_stat**2 + self.result.m_err_sys**2 ))
+        c = Data('c_intern', self.result.c, uncert_sys=np.sqrt( self.result.c_err_stat**2 + self.result.c_err_sys**2 ))
         res = ( m*(x-self.mes_mittel) + c + self.real_mittel ).consume(x_name)
         res.blacklist.remove(m)
         res.blacklist.remove(c)
         return res
 
-    def slope(self, symbol) :
-        return self.result.slope( symbol )
+    def slope(self) :
+        return self.m
 
-    def axis_intercept(self, symbol) :
-        c = self.result.axis_intercept( 'c_intern' )
-        m = self.result.slope( 'm_intern' )
-        ret = ( c - m * self.mes_mittel + self.real_mittel).consume( symbol )
-        ret.blacklist.remove( c )
-        ret.blacklist.remove( m )
-        return ret
+    def axis_intercept(self) :
+        return self.c
 
     def save_to_file(self, filen, xname, yname, font_size, legend_font_size = None,
             tex_file = None, title = None):
